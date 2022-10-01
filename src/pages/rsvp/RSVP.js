71c025212e666e774b265107e176d943f5d49941
jasonbeaver <jasonbeaverw99@gmail.com>
2022-09-30 21:06:54 -0400

    Added cosmetic changes.

--- src/pages/rsvp/RSVP.js
+++ src/pages/rsvp/RSVP.js
@@ -3,7 +3,15 @@ import CreateItem from '../../components/rsvp_list/CreateItem';
 
 function RSVP() {
   return (
-    <div>
+    <div className='rsvp-contianer'>
+      <h1>We Are Excited To Have You!</h1>
+      <h2>
+        If you have any questions about a plus one <br/>
+        feel free to email either one of us <br/>
+        jasonbeaverw99@gmail.com<br/>
+        or give us a call at <br/>
+        336-909-8493.
+      </h2>
       <CreateItem/>
     </div>
   )
