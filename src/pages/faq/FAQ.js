e64a56a8d8d4d6d6799e423cc9ad4c4d32991ddc
jasonbeaver <jasonbeaverw99@gmail.com>
2022-10-08 18:25:57 -0400

    Added better functionality to the login.

--- src/pages/faq/FAQ.js
+++ src/pages/faq/FAQ.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
+import '../faq/faq.css'
 import Login from '../../components/login/Login'
 
 export default function FAQ() {
   const [showLogin, setShowLogin] = useState(false);
-  const onClick = () => setShowLogin(true);
+  const onClick = () => {
+    showLogin == true 
+    ? setShowLogin(false) 
+    : setShowLogin(true);
+  }
 
   return (
-    <div>
+    <div className='faq-container'>
+      <div>
+      </div>
       <div className='login-holder'>
-        <span onClick={onClick}> Sign In Btn Test</span>
+        <span onClick={onClick} className="sign-in-btn">Show Login</span>
         {showLogin ? <Login /> : null }
       </div>
     </div>
